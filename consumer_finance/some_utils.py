9cf0c7f106a933014fd5321866b819fb7f5e6d48
haoran-yang <17713624036@163.com>
2019-12-05 14:39:09 +0800

    compute psi

--- consumer_finance/some_utils.py
+++ consumer_finance/some_utils.py
@@ -62,6 +62,37 @@ def code_transform(data,sys_code):
                 replace_dict[col] = replace_t
     return replace_data,replace_dict
 
+
+def data_cut(series_data, labels=False, cuts=20):
+    data_of_cut=pd.cut(series_data,bins=cuts,labels=labels,include_lowest=True)
+    datacounts=pd.value_counts(data_of_cut,sort=False)
+    return datacounts
+
+def get_psi(series_data1, series_data2):
+    '''psi值计算'''
+    data1=data_cut(series_data1)
+    data2=data_cut(series_data2)
+    data1_percent=data1.map(lambda x:x/data1.sum())
+    data2_percent=data2.map(lambda x:x/data2.sum())
+    total=pd.concat([data1_percent,data2_percent],axis=1)
+    total['div']=total.iloc[:,0]/total.iloc[:,1]
+    total['div_ln']=total['div'].apply(lambda x:math.log(x,math.e))
+    total['signal_psi']=(total.iloc[:,0]-total.iloc[:,1])*total['div_ln']
+    psi_data=total['signal_psi'].sum()
+    return psi_data
+
+def psi_oneByone(df, columnname):
+    '''psi调用函数'''
+    psi_dict={}
+    for x in df.date.unique().tolist():
+        psi_dict[x]={}
+        for y in df.date.unique().tolist():
+            data_one=df.query('date=="{}"'.format(x))[columnname]
+            data_two=df.query('date=="{}"'.format(y))[columnname]
+            psivalue=get_psi(data_one,data_two)
+            psi_dict[x][y]=psivalue
+    return pd.DataFrame(psi_dict)
+
 if __name__=='__main__':
     # 读取需要转换码值的字段
     with open('./code_trans_cols.txt','r') as f:
